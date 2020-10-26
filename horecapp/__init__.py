404ab6149a34c1ba8e12ed821308009c78a48ae2
Max <max.09@outlook.com>
2020-10-26 10:23:34 +0100

    creation table works now

--- horecapp/__init__.py
+++ horecapp/__init__.py
@@ -6,9 +6,36 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__, instance_relative_config=True)
-app.config.from_object('config')
-app.config.from_pyfile("config.py")
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+
+app.register_blueprint(views.bp)
 
 db = SQLAlchemy(app)
 
-app.register_blueprint(views.bp)
+
+
+class Visitors(db.Model):
+
+    #Columns
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    first_name = db.Column(db.String(128))
+    last_name = db.Column(db.String(128))
+    tel_number = db.Column(db.String(128))
+    email = db.Column(db.String(128))
+    date_visit = db.Column(db.String(128))
+    hour_visit = db.Column(db.String(128))
+
+
+
+class eatery_admin(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    admin_name = db.Column(db.String(128), unique = True, nullable = False)
+    password = db.Column(db.String(1024))
+
+
+print("db created.")
+db.create_all()
+print(db)
+
