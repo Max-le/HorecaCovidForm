c20c97f462cb3329fd51562f10db718165d68ae5
Max <max.09@outlook.com>
2020-11-23 06:16:06 +0100

    one-file app : regroup things

--- horecapp/__init__.py
+++ horecapp/__init__.py
@@ -1,5 +1,8 @@
+import os 
+
 from flask import Flask
-from . import views
+from flask import render_template, flash, request, redirect, url_for, Blueprint, session
+from werkzeug.security import check_password_hash, generate_password_hash
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -17,7 +20,6 @@ app.logger.info(app.config)
 
 db = SQLAlchemy(app)
 
-app.register_blueprint(views.bp)
 
 
 class Visitors(db.Model):
@@ -39,3 +41,59 @@ class Horecadmin(db.Model):
     
 db.create_all()
 app.logger.info("db created.")
+
+
+
+@app.route('/answer')
+def everything_answer():
+    return "42"
+@app.route('/')
+def show_landing_page():
+    return render_template('landing.html')
+
+@app.route('/admin', methods=('GET', 'POST'))
+def admin_login():
+    if request.method == 'POST':
+        admin_name = request.form['adminName']
+        passwordTried = request.form['password']
+        
+        error = None
+
+
+        ##Security flaw - don't indicate what information is wrong.
+        if admin is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(admin['password'], passwordTried):
+            error = "wrong pwd."
+
+
+
+        if error is None:
+            session.clear()
+            session['user_id'] = admin['id']
+            return 'Succesfull admin login !'
+        flash(error)
+    return render_template('admin.html')
+
+
+
+def get_list_visitors():
+
+    for visitor in visitors: 
+        print(tuple(visitor))
+    return "List of visitors will appear here."
+
+
+@app.route('/form', methods=['GET', 'POST'])
+def provide_form():
+
+    if request.method == "POST": 
+
+        first_name = request.form.get("firstName")
+        last_name = request.form.get("lastName")
+        email = request.form.get("email")
+        date_visit = request.form.get("dateVisit")
+        hour_visit = request.form.get("hourVisit")
+        return render_template("validated.html")
+   
+    return render_template("form.html")
